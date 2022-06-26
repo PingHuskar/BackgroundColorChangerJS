f77349c76fd0cd2e947192d439f7128de8876437
Chadin Chaipornpisuth <61967318+PingHuskar@users.noreply.github.com>
2022-06-26 14:31:02 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
 const searchParam = new URLSearchParams(window.location.search)
 var bg = searchParam.get('bg') !== null ? searchParam.get('bg') : Math.floor(Math.random()*16777215).toString(16)
-if (/[\da-f]{7,}/i.test(bg)) {
-    bg = bg.slice(0,6)
-}
-if (/[\da-f]{6}|[\da-f]{3}/i.test(bg)) {
-    document.body.style.backgroundColor = `#${bg}`
-}
-else {
-    document.body.style.backgroundColor = bg
-}
+bg = (/[\da-f]{7,}/i.test(bg)) ? bg.slice(0,6): bg
+document.body.style.backgroundColor = (/[\da-f]{6}|[\da-f]{3}/i.test(bg)) ? `#${bg}` : bg
 console.log(`bg = ${bg}`)
