437a015cc76174cfd5c7060050538d35bd0efce4
Chadin Chaipornpisuth <ping47024@icloud.com>
2022-09-16 10:18:38 +0700

    separate white and random

--- random.js
+++ random.js
@@ -1,5 +1,5 @@
 const searchParam = new URLSearchParams(window.location.search)
-var bg = searchParam.get('bg') || '#fff'
+var bg = searchParam.get('bg') !== null ? searchParam.get('bg') : Math.floor(Math.random()*16777215).toString(16)
 bg = (/[\da-f]{7,}/i.test(bg)) ? bg.slice(0,6): bg
 document.body.style.backgroundColor = (/[\da-f]{6}|[\da-f]{3}/i.test(bg)) ? `#${bg}` : bg
 console.log(`bg = ${bg}`)
